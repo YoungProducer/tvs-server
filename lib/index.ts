bcd0665e54446727e23554c05771946bf8b23ee2
YoungProducer <sashabezrukovownmail@gmail.com>
2020-05-12 01:23:01 +0300

    feat: configured cors.

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, NextFunction, Response } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import socket from 'socket.io';
 import { Server } from 'http';
 import dotenv from 'dotenv';
@@ -12,12 +13,21 @@ const app: express.Application = express();
 export const server = new Server(app);
 export const io = socket(server);
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ["POST"],
+    credentials: true,
+    maxAge: 3600,
+};
+
 const envConfig = dotenv.config() as EnvConfigInput;
 
 parseEnv(app, envConfig);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors(corsOptions));
 
 app.use('/', appRouter);
 
