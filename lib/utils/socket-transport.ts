704ffc998c4b0d901f0ca795d440e85900f2dd65
YoungProducer <sashabezrukovownmail@gmail.com>
2020-05-11 15:03:53 +0300

    feat: added feature which allows to work with namespaces.

--- lib/utils/socket-transport.ts
+++ lib/utils/socket-transport.ts
@@ -1,20 +1,44 @@
-import { Server } from 'socket.io';
+import { Server, Namespace } from 'socket.io';
 
 export namespace SocketTransport {
     export type EmitEvent = <T = any>(eventName: string, data: T, to?: string) => void;
     export type ListenEvent = (eventName: string, cb: () => void) => void;
+    export type UseNamespace = (name: string) => void;
+    export type EmitEventToNamespace = <T = any>(
+        namespaceName: string,
+        eventName: string,
+        data: T,
+        to?: string,
+    ) => void;
+    export type ListenEventOfNamespace = (
+        namespaceName: string,
+        eventName: string,
+        cb: () => void,
+        to?: string,
+    ) => void;
 
     export interface Controller {
         emitEvent: EmitEvent;
         listenEvent: ListenEvent;
+        useNamespace: UseNamespace;
+        emitEventToNamespace: EmitEventToNamespace;
+        listenEventOfNamespace: ListenEventOfNamespace;
     }
 }
 
 export class SocketTransport implements SocketTransport.Controller {
     instance: Server;
+    namespaces: Namespace[];
+    private ableToUseNamespace: boolean = false;
 
     constructor(socket: Server) {
         this.instance = socket;
+        this.namespaces = [];
+    }
+
+    useNamespace: SocketTransport.UseNamespace = (name) => {
+        const newNamespace = this.instance.of(name);
+        this.namespaces.push(newNamespace);
     }
 
     emitEvent: SocketTransport.EmitEvent = (eventName, data, to) => {
@@ -27,5 +51,37 @@ export class SocketTransport implements SocketTransport.Controller {
 
     listenEvent: SocketTransport.ListenEvent = (eventName, cb) => {
         this.instance.on(eventName, cb);
+    }
+
+    emitEventToNamespace: SocketTransport.EmitEventToNamespace = (
+        namespaceName,
+        eventName,
+        data,
+        to,
+    ) => {
+        const namespaceIntance = this.namespaces.find(namespace =>
+            namespace.name = namespaceName);
+
+        if (to && namespaceIntance) {
+            namespaceIntance.to(to).emit(eventName, data);
+        } else if (!to && namespaceIntance) {
+            namespaceIntance.emit(eventName, data);
+        }
+    }
+
+    listenEventOfNamespace: SocketTransport.ListenEventOfNamespace = (
+        namespaceName,
+        eventName,
+        cb,
+        to,
+    ) => {
+        const namespaceIntance = this.namespaces.find(namespace =>
+            namespace.name = namespaceName);
+
+        if (to && namespaceIntance) {
+            namespaceIntance.to(to).on(eventName, cb);
+        } else if (!to && namespaceIntance) {
+            namespaceIntance.on(eventName, cb);
+        }
     }
 }
