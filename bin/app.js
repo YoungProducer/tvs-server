0dec43230dab6a74002799f17be906cc3a530c29
YoungProducer <sashabezrukovownmail@gmail.com>
2020-05-13 11:51:09 +0300

    feat: updated start script, changed path in bin/app.js.

--- bin/app.js
+++ bin/app.js
@@ -1,4 +1,6 @@
-const { app, server } = require('../lib/index');
+const production = process.env.NODE_ENV === 'production';
+
+const { app, server } = require(production ? '../dist/lib/index' : '../lib/index');
 
 server.listen(app.get('PORT'), () => {
     console.log(`App listening to ${app.get('PORT')}...`, `mode: ${app.get('env')}`);
