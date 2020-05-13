a05de2981e52b6a5e6156467af8b995f430b5435
YoungProducer <sashabezrukovownmail@gmail.com>
2020-05-13 12:08:05 +0300

    feat: changed hostname.

--- bin/app.js
+++ bin/app.js
@@ -2,6 +2,6 @@ const production = process.env.NODE_ENV === 'production';
 
 const { app, server } = require(production ? '../dist/lib/index' : '../lib/index');
 
-server.listen(app.get('PORT'), () => {
+server.listen(app.get('PORT'), '0.0.0.0', () => {
     console.log(`App listening to ${app.get('PORT')}...`, `mode: ${app.get('env')}`);
 });
